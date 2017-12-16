6517f80147da45b67868925c33530f8af167b11c
MooreDA <31998413+MooreDA@users.noreply.github.com>
2017-12-15 18:27:37 -0600

    Actualizacion
    Cambios en el progrma

--- Proyecto final - Don Lee/src/sample/Controller2.java
+++ Proyecto final - Don Lee/src/sample/Controller2.java
@@ -2,7 +2,11 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class Controller2 {
     @FXML
@@ -20,22 +24,52 @@ public class Controller2 {
 
 
     public void agregar1(ActionEvent actionEvent) {
-        
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Pilla tu pedido!");
+        alerta.setContentText("Pedido en proceso");
+        alerta.setHeaderText("Su precio a pagar: 7.95");
+        Optional<ButtonType> resultado = alerta.showAndWait();
     }
 
     public void agregar2(ActionEvent actionEvent) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Pilla tu pedido!");
+        alerta.setContentText("Pedido en proceso");
+        alerta.setHeaderText("Su precio a pagar: 7.85");
+        Optional<ButtonType> resultado = alerta.showAndWait();
     }
 
     public void agregar3(ActionEvent actionEvent) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Pilla tu pedido!");
+        alerta.setContentText("Pedido en proceso");
+        alerta.setHeaderText("Su precio a pagar: 8.05");
+        Optional<ButtonType> resultado = alerta.showAndWait();
     }
 
     public void agregar4(ActionEvent actionEvent) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Pilla tu pedido!");
+        alerta.setContentText("Pedido en proceso");
+        alerta.setHeaderText("Su precio a pagar: 7.25");
+        Optional<ButtonType> resultado = alerta.showAndWait();
     }
 
+
     public void agregar5(ActionEvent actionEvent) {
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+            alerta.setTitle("Pilla tu pedido!");
+            alerta.setContentText("Pedido en proceso");
+            alerta.setHeaderText("Su precio a pagar: 7.65");
+            Optional<ButtonType> resultado = alerta.showAndWait();
     }
 
     public void agregar6(ActionEvent actionEvent) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Pilla tu pedido!");
+        alerta.setContentText("Pedido en proceso");
+        alerta.setHeaderText("Su precio a pagar: 6.75");
+        Optional<ButtonType> resultado = alerta.showAndWait();
     }
 }
 
